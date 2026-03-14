bd15fae98cabb9a6d5fed400114da2008f080140
Prasanna-Sivakumar <prasannasivakumar2006@gmail.com>
2026-03-14 19:34:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function filterMenu(category){
+
+let items = document.querySelectorAll(".menu-item")
+
+items.forEach(item => {
+
+if(category === "all"){
+item.style.display = "block"
+}
+
+else if(item.classList.contains(category)){
+item.style.display = "block"
+}
+
+else{
+item.style.display = "none"
+}
+
+})
+
+}
